2386857c53f9eb3d8bebb5e4899cd2f73cfbe75e
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-11-30 20:26:49 +0100

    upgraded resources slurm

--- submit_trainable.sh
+++ submit_trainable.sh
@@ -2,14 +2,14 @@
 #SBATCH --time=04:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=24
 #SBATCH --job-name=CAPSNET
 #SBATCH --mail-type ALL
 #SBATCH --mail-user jos.vandewolfshaar@gmail.com
 #SBATCH --output CAPSNET-%j.log
 #SBATCH --mem=16000
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 
 module load tensorflow/1.3.1-foss-2016a-Python-3.5.2-CUDA-8.0.61
 source envs/lws/bin/activate
